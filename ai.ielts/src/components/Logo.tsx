392ca55d2f212f81f2a89068bd65809ca5fa3a12
aibar <akenbay@icloud.com>
2025-01-04 14:30:59 +0500

    Created a card for full test

---
+++ ai.ielts/src/components/Logo.tsx
@@ -0,0 +1,5 @@
+function Logo(){
+    return <div style={{color: "#EF601E"}} className="row"> <div className="col"><img src="/logo.png" alt="" /></div> <div className="col"><h1>aielts.</h1></div> </div>;
+}
+
+export default Logo;
