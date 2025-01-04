86722ac95d7270179e36a6b2485f382987634fe5
aibar <akenbay@icloud.com>
2025-01-04 18:53:57 +0500

    Created new vite app and added shadcnui

--- ai.ielts/src/components/Logo.tsx
+++
@@ -1,5 +0,0 @@
-function Logo(){
-    return <div style={{color: "#EF601E"}} className="row"> <div className="col"><img src="/logo.png" alt="" /></div> <div className="col"><h1>aielts.</h1></div> </div>;
-}
-
-export default Logo;
